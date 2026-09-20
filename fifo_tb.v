5816f3e824d7630f7fda912d8455493091727835
SaiNageshyr <125950769+SaiNageshyr@users.noreply.github.com>
2026-09-20 15:17:43 +0530

    Update fifo_tb.v

--- fifo_tb.v
+++ fifo_tb.v
@@ -20,13 +20,13 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module fifo_tb();
+module fifo_2clks_tb();
 
-	reg wr_en, rd_en, clk, rst;
+	reg wr_en, rd_en, clk1,clk2, rst;
 	reg [7:0] data_in;
 	wire empt, full;
 	wire [7:0] data_out;
-	fifo tb( wr_en, rd_en, clk, rst, data_in, empt, full, data_out);
+	fifo_2clks tb( wr_en, rd_en, clk1, clk2, rst, data_in, empt, full, data_out);
 	
 	initial 
 	begin
@@ -38,20 +38,21 @@ module fifo_tb();
 		data_in = 8'd0;
 		#10 rst = 1'b0;
 		wr_en=1'b1;
-		#10 data_in = 4;
+		data_in = 4;
 		#14 data_in = 5;
 		#14 data_in = 6;
 		#14 wr_en=1'b0;
-		#15 rd_en=1'b1;
-		#20 rd_en=1'b0;
 		#15 rd_en=1'b1;
 		#20 rd_en=1'b0;
 		#50 $finish;
 	end
 	initial
 	  begin
-	     clk    = 1'b0;
+	     clk1    = 1'b0;
+	     clk2    = 1'b0;
 	  end
-	always #5 clk    = ~ clk;
-	
+	always begin 
+	#1.665 clk1    = ~ clk1;
+	#5     clk2    = ~ clk2;
+	end
 endmodule
