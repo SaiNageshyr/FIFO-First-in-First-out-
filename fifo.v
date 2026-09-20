bab018b4b45479853f5467ccc3bf61861355571a
SaiNageshyr <125950769+SaiNageshyr@users.noreply.github.com>
2026-09-20 15:16:59 +0530

    Update fifo.v

--- fifo.v
+++ fifo.v
@@ -20,46 +20,45 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module fifo( 
-	input wr_en, rd_en, clk, rst,
+module fifo_2clks( 
+	input wr_en, rd_en, clk1,clk2, rst,
 	input [7:0] data_in,
 	output reg empt, full,
 	output reg [7:0] data_out
 
     );
-	//parameter FIFO_DEPTH = 8'd7, COUNTER_SIZE = 3'd3;
+	parameter FIFO_DEPTH = 8'd8, COUNTER_SIZE = 3'd3;
 	
-	reg [6:0]wr_ptr, rd_ptr;
-	reg [6:0] count;
-	reg [7:0] mem [7:0];
-	
-	always @(posedge rst)
-        begin
-            empt <= 1'b0;
-            full <= 1'b0;
-        end
-	always @(posedge clk)
-	begin
-		empt <= (count== 0)?1'b1:1'b0;
-		full <= (count== 7'b1000000)?1'b1:1'b0;
+	reg [COUNTER_SIZE - 1:0]wr_ptr, rd_ptr;
+	wire [COUNTER_SIZE - 1:0] count;
+	reg [7:0] mem [FIFO_DEPTH - 1:0];
+	integer i,j;
+	initial begin 
+	   for (i=0; i<FIFO_DEPTH ;i=i+1) begin 
+	       for (j=0; j<FIFO_DEPTH ;j=j+1)
+	           mem[i][j]<=1'b0;
+	   end
 	end
 	
-	always @(posedge clk, posedge rst)
+    assign count = (wr_ptr - rd_ptr) % FIFO_DEPTH;
+    
+	always @(posedge clk1, posedge rst)
 	begin
-		if(rst)
-			count<=0;
-		else
-		begin
-			case ({wr_en, rd_en})
-				2'b00 : count <= count;
-				2'b01 : count <= (!empt) ? count - 1 : count;
-				2'b10 : count <= (!full) ? count + 1 : count;
-				2'b11 : count <= count;
-				default : count <= count;
-			endcase
-		end
-		end
-	always @ ( posedge clk, posedge rst)
+	if (rst)
+	   full<=0;
+    else
+		full <= (count== FIFO_DEPTH - 1)?1'b1:1'b0;
+	end
+	
+	always @(posedge clk2, posedge rst)
+        begin
+        if (rst)
+            empt<=0;
+        else
+            empt <= (count== 0)?1'b1:1'b0;
+        end
+
+	always @ ( posedge clk2, posedge rst)
 	begin
 		if(rst)
 			data_out <= 0;
@@ -72,7 +71,7 @@ module fifo(
 		end
 	end
 	
-	always @ ( posedge clk)
+	always @ ( posedge clk1)
 	begin
 		if( wr_en && !full)
 			mem[wr_ptr] <= data_in;
@@ -80,22 +79,31 @@ module fifo(
 			mem[wr_ptr] <= mem[wr_ptr] ;
 	end
 	
-	always @ ( posedge clk, posedge rst)
-	begin
-		if(rst)begin
-			wr_ptr <= 0;
-			rd_ptr <= 0;
-		end
-		else
-		begin
-			if ( !full && wr_en)
-				wr_ptr <= wr_ptr + 1;
-			else
-				wr_ptr <= wr_ptr;
-			if (!empt && rd_en)
-				rd_ptr <= rd_ptr + 1;
-			else 
-				rd_ptr <= rd_ptr;
-		end
-		end
+	
+	always @ ( posedge clk1, posedge rst)
+            begin
+                if(rst)begin
+                    wr_ptr <= 0;
+                end
+                else
+                begin
+                    if ( !full && wr_en)
+                        wr_ptr <= wr_ptr + 1;
+                    else
+                        wr_ptr <= wr_ptr;
+                end
+                end
+    always @ ( posedge clk2, posedge rst)
+            begin
+                if(rst)begin
+                    rd_ptr <= 0;
+                end
+                else
+                begin
+                    if (!empt && rd_en)
+                        rd_ptr <= rd_ptr + 1;
+                    else 
+                        rd_ptr <= rd_ptr;
+                end
+                end
 endmodule
